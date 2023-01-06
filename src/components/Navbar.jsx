fa8fa61e6d8dceac78efc547ad2e21cac753d10c
Sebin Thomas <sthomas@thedoctors.com>
2023-01-06 14:55:02 -0500

    [~] Removed Sign In - REFTO

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -262,7 +262,7 @@ export default function Example() {
               </Popover>
             </Popover.Group>
             <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-            <Link to="/dashbaords" className="whitespace-nowrap text-base font-medium
+            <Link to="#" className="whitespace-nowrap text-base font-medium
              text-white hover:text-gray-900" onClick={handleLogin}>
                 Sign in
               </Link>
