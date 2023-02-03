799d1ae05a6ad7416169f0218e0d26934c1bdcf0
Anthony <aezeji00@gmail.com>
2023-02-03 14:20:36 -0500

    video hero

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,58 @@
+
+	import React from 'react'
+    import '../css/Carousel.css' 
+    import BHSLogo from'../assets/client logos/Bronx Health System.png'
+    import MSHLogo from '../assets/client logos/Mount Sinai.jpg'
+    import MFLogo from'../assets/client logos/Montefiore.png'
+    import NyackLogo from '../assets/client logos/Nyack.jpg'
+    import WPLogo from'../assets/client logos/White Plains.png'
+    import BlytheLogo from '../assets/client logos/Blythedale.png'
+    import STLukeLogo from '../assets/client logos/St Luke.png'
+    function Carousel() {
+      return (
+        <div  class="marquee">
+          
+        <ul class="marquee-content">
+         
+        <li  className='px-4 py-0 '><img src={BHSLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MFLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={NyackLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={WPLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={BlytheLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={STLukeLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={STLukeLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={STLukeLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MSHLogo} alt="" /></li>
+        <li className='px-4 py-0 '><img src={MFLogo} alt="" /></li>
+
+        
+        
+        
+    
+        </ul>
+   
+      </div>
+      )
+    }
+    
+    export default Carousel
+    
+    
