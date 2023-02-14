6b0ae1c39269820286b531fa545c606c07abea2b
Anthony <aezeji00@gmail.com>
2023-02-14 16:11:09 -0500

    Implemented gurarded api routes

--- src/components/Dashboards.jsx
+++ src/components/Dashboards.jsx
@@ -101,7 +101,12 @@ const [savedReports, setSavedReports] = useState([])
 const [savedReportSelected, setSavedReportSelected] = useState(false)
 const [commentInputText, setCommentInputText] = useState('')
 const [selectedReport, setSelectedReport] = useState(null)
-
+const config = {
+  headers:{
+    accesstoken: accessToken,
+  
+  }
+};
 
 //Sets auth state to true when user is authenticated
 useEffect(() => {
@@ -184,40 +189,40 @@ function timeSince(date) {
   return Math.floor(seconds) + " second(s)";
 }
 var aDay = 24*60*60*1000;
-//Sets reports state to api request response for reports
-useEffect(() => {
-async function getReports(){
-  await axios.get('https://hra-backend-q2gs-atz7s8hi9-anthonyezeji.vercel.app/reports').then(res=>{
+const axiosInstance = axios.create({
 
+  baseURL: 'https://hra-backend-q2gs-atz7s8hi9-anthonyezeji.vercel.app',
+  timeout: 1000
+});
+
+
+
+async function getReports(){ 
+  await axiosInstance.get(`/reports?token=${accessToken}`).then(res=>{
+    console.log(res.data)
     setReports(res.data)
     
   })
 }
-try {
 
-  getReports()
-} catch (error) {
-  console.log(error)
+ async function getSavedReports(){
+
+     await axiosInstance.get(`/users/${instance.getActiveAccount()?.idTokenClaims?.oid}/reports?token=${accessToken}`).then(res=>{
+
+       setSavedReports(res.data)
+     
+     })
 }
 
-}, [])
-//When a user is authenticated, an api request is sent for the users saved reports
-useEffect(() => {
+
   
-   async function getSavedReports(){
  
-       await axios.get(`https://hra-backend-q2gs-atz7s8hi9-anthonyezeji.vercel.app/users/${instance.getActiveAccount()?.idTokenClaims?.oid}/reports`).then(res=>{
-
-         setSavedReports(res.data)
-       
-       })
-  }
-getSavedReports()
+ 
 
 
 
 
-}, [authenticated])
+
 
 //function accepts a report as a parameter and returns whether the current user has it saved
 function checkIfSaved(report){
@@ -263,7 +268,7 @@ let account = instance.getActiveAccount()
  }
  //function handles a user saving a report
 async function handleSaveReport(){
-  await axios.post(`https://hra-backend-q2gs-atz7s8hi9-anthonyezeji.vercel.app/users/${instance.getActiveAccount().idTokenClaims?.oid}/reports`,{...selectedReport,user:instance.getActiveAccount()}).then(res=>{
+  await axiosInstance.post(`/users/${instance.getActiveAccount().idTokenClaims?.oid}/reports?token=${accessToken}`,{...selectedReport,user:instance.getActiveAccount()}).then(res=>{
     
     setSavedReportSelected(!savedReportSelected)
     
@@ -274,7 +279,7 @@ async function handleSaveReport(){
  //function handles user removing report from their saved reports
  async function removeSavedReport(){
   
-  await axios.delete(`https://hra-backend-q2gs-atz7s8hi9-anthonyezeji.vercel.app/users/${instance.getActiveAccount()?.idTokenClaims?.oid}/reports`, {data:selectedReport}).then((res)=>{
+  await axiosInstance.delete(`/users/${instance.getActiveAccount()?.idTokenClaims?.oid}/reports?token=${accessToken}`, {data:selectedReport}).then((res)=>{
 
   
   setSavedReportSelected(!savedReportSelected)
@@ -288,22 +293,23 @@ async function handleSaveReport(){
 }
 //When an access token is set, we make a request to ms graph for the user's groups
  useEffect(() => {
-
    RequestAccessToken()
+
  }, [isAuthenticated])
- const [userPhoto, setUserPhoto] = useState('');
- useEffect(() => {
-  if(isAuthenticated){
 
-  
-
-    async function getUserGroups(){
-     await callMsGraph(accessToken).then(response=>{
-  
-       setUserGroups((response.value))
-      })
-    }
-    
+ useEffect( () => {
+ 
+  async function getUserGroups(){
+    await callMsGraph(accessToken).then(response=>{
+ 
+      setUserGroups((response.value))
+     })
+   }
+ 
+  if(accessToken){
+    console.log(accessToken)
+    getReports()
+    getSavedReports()
     getUserGroups()
   }
  }, [accessToken])
@@ -322,12 +328,12 @@ async function handleSaveReport(){
  }
  //When the user's groups are set, we filter the reports
  useEffect(() => {
-  
+  console.log(reports)
   if(userGroups?.length>0&&reports.length>0){
     setFilteredReports(reports?.filter(filterReports))
   }
    
- }, [userGroups])
+ }, [userGroups,reports])
 
 
 
@@ -338,7 +344,7 @@ async function handleSaveReport(){
     
     return
   }else{
-    
+
     if(window.sessionStorage.hasOwnProperty('selectedReport')){
      var sessionReport = JSON.parse(window.sessionStorage.getItem('selectedReport'))
       if(sessionReport!==null){
