6d521c545917ca2602be9c49930e467efbe347cb
Anthony <aezeji00@gmail.com>
2023-02-23 13:00:09 -0500

    comment

--- src/components/personas/PersonaBlog.jsx
+++ src/components/personas/PersonaBlog.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import Navbar from '../Navbar'
 import { models } from 'powerbi-client';
-//map for persona-blog content
+//map for persona-blog content with routes as keys
 const dashboards = {'/risk-manager/med-mal':{reportId:'12345',content:' content for med mal', caption:'Hospital Med Mal Overview'},'/risk-manager/freq-sev-trends':{reportId:'12346',content:'content for freq/sev trends', caption:'Frequency/Severity Trends'},'/risk-manager/national-benchmark':{reportId:'123467',content:'conent for national benchmarking', caption:'National Benchmarking'}}
 function PersonaBlog() {
     const [location, setLocation] = useState('')
