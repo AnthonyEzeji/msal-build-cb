4238f49e6b43e1d4490d55ef085e4eba1e1a9231
Anthony <aezeji00@gmail.com>
2023-02-27 18:20:45 -0500

    further persona implementation

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import LoginPage from "./components/LoginPage";
 import RiskManager from "./components/personas/RiskManager";
 import DepartmentChair from "./components/personas/DepartmentChair";
 import PersonaBlog from "./components/personas/PersonaBlog";
+import CXO from "./components/personas/CXO";
 const About = () => {
   return <h1>About</h1>
 };
@@ -36,11 +37,18 @@ function App() {
         <Route path='/' element = {<HomePage/>}/>
         <Route path='/login' element = {<LoginPage/>}/>
         <Route path='/risk-manager/med-mal' element = {<PersonaBlog/>}/>
-        <Route path='/risk-manager/freq-sev-trends' element = {<PersonaBlog/>}/>
-        <Route path='/risk-manager/national-benchmark' element = {<PersonaBlog/>}/>
+        <Route path='/risk-manager/frequency-severity-trends' element = {<PersonaBlog/>}/>
+        <Route path='/risk-manager/national-benchmarking' element = {<PersonaBlog/>}/>
+        <Route path='/department-chair/freq-flyers' element = {<PersonaBlog/>}/>
+        <Route path='/department-chair/icd-level-malpractice-insights' element = {<PersonaBlog/>}/>
+        <Route path='/department-chair/med-mal-contributing-factors' element = {<PersonaBlog/>}/>
+        <Route path='/cxo/kpi-boardroom' element = {<PersonaBlog/>}/>
+        <Route path='/cxo/financial-benchmarking' element = {<PersonaBlog/>}/>
+        <Route path='/cxo/department-insights' element = {<PersonaBlog/>}/>
         <Route path='/get-started' element = {<StepsPage/>}/>
         <Route path='/risk-manager' element = {<RiskManager/>}/>
         <Route path='/department-chair' element = {<DepartmentChair/>}/>
+        <Route path='/cxo' element = {<CXO/>}/>
         <Route path='/solutions/hospitals/data-analytics' element = {<AnalyticsPage/>}/>
         <Route path='/dashboard' element = {<Dashboards/>}/>
         <Route path='/saved-reports' element = {<SavedReports/>}/>
--- src/components/personas/RMPreviewButtons.jsx
+++ src/components/personas/RMPreviewButtons.jsx
@@ -1,54 +1,65 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-function PreviewButtons() {
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import img from '../../assets/Risk management-rafiki.svg'
+const features = [
+  {
+    name: 'Hospital Med Mal Overview',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: CloudArrowUpIcon,
+    href:'/risk-manager/med-mal'
+  },
+  {
+    name: 'Frequency/Severity Trends',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+    href:'/risk-manager/frequency-severity-trends'
+  },
+  {
+    name: 'National Benchmarking',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ServerIcon,
+    href:'/risk-manager/national-benchmarking'
+  },
+]
+
+export default function Example() {
   return (
-    <div className='w-screen flex justify-center flex-col items-center justify-center  min-h-screen mb-10 md:mb-0'>
-        <div className='w-[90vw] flex justify-center 2xl:justify-end'>
-        <div className=' 2xl:w-[70vw]  flex flex-col  '>
-        <h1 className=' text-2xl md:text-3xl 2xl:text-4xl  font-bold text-center lg:text-left py-10 w-full'>Get the Insights to stay ahead of change</h1>
-        <div className='flex flex-col lg:flex-row items-center lg:items-start h-full w-full'>
-            <img className='w-full md:w-[450px] 2xl:w-[450px] xl:max-w-[450px] md:rounded-md' src="https://as1.ftcdn.net/v2/jpg/03/38/49/28/1000_F_338492830_gOno5hqTomxyaP4RZd9ugMzlt1cZ8Qte.jpg" alt="" />
-            <div className='  flex flex-col items-start  w-full h-full  '>
-                <div className='flex flex-1 justify-center items-start mb-2 w-full md:w-fit flex-col md:flex-row md:px-5 py-3    '>
-                    <div>
-                        <h1 className='text-2xl font-bold w-fit md:max-w-[400px] '>
-                        Hospital Med Mal Overview
-                        </h1>
-                        <p className='text-gray-500 md:w-[400px] '>The strategy that leads to a new perforance frontier</p>
-                    </div>
-
-                    <Link to ='/risk-manager/med-mal' className='px-10 max-h-[50px] md:ml-5 text-white w-full md:w-fit bg-red-600 rounded-md p-3'>Preview</Link>
-                </div>
-                <div className='flex flex-1 justify-center items-start mb-2 w-full md:w-fit flex-col md:flex-row md:px-5 py-3   '>
-                    <div>
-                        <h1 className='text-2xl font-bold w-fit md:max-w-[400px]'>
-                        Frequency / Severity Trends
-                        </h1>
-                        <p className='text-gray-500 md:w-[400px] '>Focusing the power of data, tech and people to accelerate reinvention—and a new vision for growth</p>
-                    </div>
-
-                    <Link to='/risk-manager/freq-sev-trends' className='px-10 max-h-[50px] md:ml-5 text-white w-full md:w-fit bg-red-600 rounded-md p-3'>Preview</Link>
-                </div>
-                <div className='flex flex-1 justify-center items-start mb-2 w-full md:w-fit flex-col md:flex-row md:px-5 py-3  '>
-                    <div >
-                        <h1 className='text-2xl font-bold w-fit md:max-w-[400px]   '>
-                        National benchmarking
-                        </h1>
-                        <p className='text-gray-500 md:w-[400px]  '>Gradually, then suddenly: The metaverse is changing the way we experience the internet</p>
-                    </div>
-
-                    <Link to='/risk-manager/national-benchmark'  className='px-10 max-h-[50px] md:ml-5 text-white w-full md:w-fit bg-red-600 rounded-md p-3'>Preview</Link>
-                </div>
-              
+    <div className="overflow-hidden bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <h2 className="text-base font-semibold leading-7 text-red-600">Mitigate Risk</h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Risk Manager</p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                iste dolor cupiditate blanditiis ratione.
+              </p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-gray-900">
+                      <feature.icon className="absolute top-1 left-1 h-5 w-5 text-red-600" aria-hidden="true" />
+                      {feature.name}
+                    </dt>{' '}
+                    <dd className="inline">{feature.description}<a href = {feature.href} className='text-red-500 mx-2 font-semibold hover:border-none border-b border-red-500'>Learn more.</a></dd>
+                    
+                  </div>
+                ))}
+              </dl>
             </div>
+          </div>
+          <img
+            src={img}
+            alt="Product screenshot"
+            className="w-[48rem] max-w-[600px] rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            width={2432}
+            height={1442}
+          />
         </div>
+      </div>
     </div>
-        </div>
- 
-    </div>
-   
   )
 }
-
-export default PreviewButtons
