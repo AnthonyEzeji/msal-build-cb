4ea005064e78a2b54534f77d78196b4e8e6492c1
Sebin Thomas <sthomas@thedoctors.com>
2023-07-26 16:41:06 +0000

    Update

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ChatWindow() {
   return (
     <div className='flew w-screen h-screen bg-white'>
-        <iframe className='w-full h-full' src="https://www.bing.com" frameborder="0"></iframe>
+        <iframe className='w-full h-full' src="https://ngagpt.azurewebsites.net/" frameborder="0"></iframe>
     </div>
   )
 }
